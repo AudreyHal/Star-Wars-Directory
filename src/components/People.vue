664b08b6bd3bb7e2ff78d284c068104ba2ae9312
AudreyHal <chiomahalim@yahoo.com>
2019-05-25 17:14:56 +0100

    Mount swapi in vue components

---
+++ src/components/People.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+
+ <h1>{{info}}</h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'People',
+  data () {
+    return {
+     info:[]
+    }
+  },
+    mounted () {
+    var axios = require('axios');
+    axios({  method: 'get', url: 'https://swapi.co/api/people',   })
+      .then(response => (this.info = response.data.results[1])  )
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,66 +1,28 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-//import App from './App'
+import People from './components/People.vue'
+////import App from './App'
 var axios = require('axios');
 require('./assets/css/main.css')
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+ components:{People},
   data () {
     return {
-      info: null,
-      temp: null,
-      mesage:"yhyy",
-      date: null,
-      hours: null,
-      results:null,
-      time:null,  
-      day1temp:null,
-      todayNumberInWeek :new Date().getDay(),
-      days :['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-     
+      
 
      
   
     }
   },
-  mounted () {
-    axios
-      .get('http://api.openweathermap.org/data/2.5/forecast?q=London&appid=770fbbdbd0524198c582803866462505' )
-      .then(
-        
-        response => (
-  
-        this.results=response.data.list,
-      this.temp = response.data.list[0].dt,
-      this.day1temp = response.data.list[0].main.temp,
-      this.date = new Date(response.data.list[0].dt_txt).getDay()
-    ))
-  },
+
   computed:{
-convert : function(){
-this.time= this.results
-return this.time},
-
-day1 : function(){
-
-
-    const date = new Date().getDay()
-
-console.log(this.date - 1)
-return(this.days[this.date-1])
-},
-
-
-
-day2 : function(){
-  console.log(this.date + 1)
- return(this.days[this.date +1])
-}
 
   }
 })
